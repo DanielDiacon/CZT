d4141c293ad740e41214ca41a1770b83ebdf78bb
Diacon Daniel <daniell.diacon@gmail.com>
2024-11-10 23:00:20 +0200

    last version

---
+++ L-5/index.js
@@ -0,0 +1,57 @@
+//// 1
+//console.log("ex 1 ------------------");
+//
+//let a = 10;
+//let b = 24;
+//
+//console.log("Suma: " + (a + b));
+//
+//// 2
+//console.log("ex 2 ------------------");
+//
+//for (let i = 1; i <= 11; i++) {
+//   if (i % 2 == 0) {
+//      console.log(i);
+//   }
+//}
+//
+//// 3
+//console.log("ex 3 ------------------");
+//
+//const factorial = (f) => {
+//   let acc = 1;
+//   for (let i = 1; i <= f; i++) {
+//      acc = acc * i;
+//   }
+//   return acc;
+//};
+//
+//let f = 5;
+//console.log(factorial(f));
+//
+//console.log("ex 4 ------------------");
+//let p = 41;
+//
+//const prim = (p) => {
+//   for (let i = 2; i * i <= p; i++) {
+//      if (p % i == 0) {
+//         return console.log("Nu este prim");
+//      }
+//   }
+//   return console.log("Este prim");
+//};
+//
+//p = 41
+//console.log(prim(p));
+//p = 42
+//console.log(prim(p));
+
+console.log("ex 5 ------------------");
+let num = 124356;
+
+const inv = (n) => {
+   //while()
+   return console.log("Este prim");
+};
+
+console.log(inv(p));
